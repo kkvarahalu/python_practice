a4d913cdaf3fbbb2508b0d3dc4f172cf3c4862a5
kkvarahalu <kalimikanakavarahalu1@gmail.com>
2024-12-27 10:35:37 +0530

    addeded new file that is addition

---
+++ add.py
@@ -0,0 +1,18 @@
+a=3
+print(a)
+c=3
+a=5
+print(a+c)
+s=3
+print(s,type(s))
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print(a + b)
+a=input("enter any number: ")
+print(type(a))
+a=input("enter first number: ")
+b=input("enter second number: ")
+print(a+b)
+
+
+
